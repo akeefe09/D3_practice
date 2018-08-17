98d6a9c19f3784ee1bcd290d742839ac47489e96
Alexander Keefe <akeefe09@yahoo.com>
2018-08-17 18:24:49 -0400

    Add axis

--- area.js
+++ area.js
@@ -1,12 +1,27 @@
 var dataArray = [25,26,28,32,37,45,55,70,90,120,135,150,160,168,172,177,180];
 var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016'];
 
+var parseDate = d3.timeParse("%Y");
+
 var height = 200;
 var width = 500;
 
-var y = d3.scaleLinear().domain([0,180]).range([height,0]);
+var margin = {
+  left: 50,
+  right: 50,
+  top: 40,
+  bottom: 0
+};
 
-console.log(y(0));;
+var y = d3.scaleLinear()
+  .domain([0,d3.max(dataArray)])
+  .range([height,0]);
+var x = d3.scaleTime()
+  .domain([])
+  .range([0,width]);
+
+var yAxis = d3.axisLeft(y).ticks(3).tickSize(10);
+var xAxis = d3.axisBottom(x);
 
 var area = d3.area()
   .x(function(d,i){ return i*20; })
@@ -16,4 +31,11 @@ var area = d3.area()
 var svg = d3.select("body").append("svg").attr("height","100%")
   .attr("width","100%");
 
-svg.append("path").attr("d",area(dataArray));
+var chartGroup = svg.append("g")
+  .attr("transform","translate("+margin.left+","+margin.top+")");
+
+chartGroup.append("path").attr("d",area(dataArray));
+
+chartGroup.append("g")
+  .attr("class","axis y")
+  .call(yAxis);
