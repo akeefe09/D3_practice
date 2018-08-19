b959fe62c5ab06574080815e09b3496f853164d9
Alexander Keefe <akeefe09@yahoo.com>
2018-08-19 11:48:35 -0400

    Add data handlers

--- area.js
+++ area.js
@@ -3,43 +3,36 @@ var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008',
 
 var parseDate = d3.timeParse("%Y");
 
+
 var height = 200;
 var width = 500;
 
-var margin = {
-  left: 50,
-  right: 50,
-  top: 40,
-  bottom: 0
-};
+var margin = {left:50,right:50,top:40,bottom:0};
 
 var y = d3.scaleLinear()
-  .domain([0,d3.max(dataArray)])
-  .range([height,0]);
+            .domain([0,d3.max(dataArray)])
+            .range([height,0]);
 var x = d3.scaleTime()
-  .domain(d3.extent(dataYears,function(d) { return parseDate(d)} ))
-  .range([0,width]);
+            .domain(d3.extent(dataYears,function(d){ return parseDate(d); }))
+            .range([0,width]);
 
-var yAxis = d3.axisLeft(y).ticks(3).tickSize(10);
+
+
+var yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
 var xAxis = d3.axisBottom(x);
 
 var area = d3.area()
-  .x(function(d,i){ return x(parseDate(dataYears[i])); })
-  .y0(height)
-  .y1(function(d){ return y(d); });
-
-var svg = d3.select("body").append("svg").attr("height","100%")
-  .attr("width","100%");
-
-var chartGroup = svg.append("g")
-  .attr("transform","translate("+margin.left+","+margin.top+")");
+                .x(function(d,i){ return x(parseDate(dataYears[i])); })
+                .y0(height)
+                .y1(function(d){ return y(d); });
+var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
+var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
 
 chartGroup.append("path").attr("d",area(dataArray));
-
 chartGroup.append("g")
-  .attr("class","axis y")
-  .call(yAxis);
+      .attr("class","axis y")
+      .call(yAxis);
 chartGroup.append("g")
-  .attr("class", "axis x")
-  .attr("transform","translate(0,"+height+")")
-  .call(xAxis);
+      .attr("class","axis x")
+      .attr("transform","translate(0,"+height+")")
+      .call(xAxis);
